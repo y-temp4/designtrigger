132418497e1f73beac8e9a220a6a981eec8a1443
y-temp4 <y.temp4@gmail.com>
2017-05-19 10:02:44 +0900

    Show error messages when submit user data with errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     if @user.save
       render json: @user.id
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
--- client/javascripts/components/UserCreate.jsx
+++ client/javascripts/components/UserCreate.jsx
@@ -6,6 +6,10 @@ export default class UserCreate extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      errors: [],
+    }
   }
 
   handleSubmit(event) {
@@ -15,7 +19,10 @@ export default class UserCreate extends React.Component {
         username: event.target.username.value,
         email: event.target.email.value,
         password: event.target.password.value,
-      }
+      },
+    }).catch((error) => {
+      console.log(error.response.data)
+      this.setState({ errors: error.response.data })
     })
   }
 
@@ -25,11 +32,15 @@ export default class UserCreate extends React.Component {
         <div className="form">
           <form onSubmit={this.handleSubmit.bind(this)}>
             <h2>ユーザー作成</h2>
+            { this.state.errors.length > 1 ?
+              this.state.errors.map((error) => {
+                return <span>{error}<br /></span>
+              }) : <div /> }
             <input type="text" name="username" required autoFocus placeholder="ユーザーID (半角英数)" />
             <br />
             <input type="email" name="email" required placeholder="メールアドレス" />
             <br />
-            <input type="password" name="password" required placeholder="パスワード (8文字以上)" />
+            <input type="password" name="password" required minLength='8' placeholder="パスワード (8文字以上)" />
             <br />
             <br />
             <button className="button">送信</button>
