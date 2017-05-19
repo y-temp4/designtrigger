4ccfb292b32bfbf2de2dbdd1aebfe584620ed7a9
y-temp4 <y.temp4@gmail.com>
2017-05-19 13:17:35 +0900

    Fix to use PropTypes via prop-types package

--- client/javascripts/components/Layout.jsx
+++ client/javascripts/components/Layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Header from './Header.jsx'
 
 export default class Layout extends React.Component {
   static contextTypes = {
-    rootProps: React.PropTypes.any,
+    rootProps: PropTypes.any,
   }
 
   render() {
--- client/javascripts/components/Link.jsx
+++ client/javascripts/components/Link.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class Link extends React.Component {
 
   static contextTypes = {
-    onLinkClick: React.PropTypes.func,
+    onLinkClick: PropTypes.func,
   }
 
   onClick(event) {
