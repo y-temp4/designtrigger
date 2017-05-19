576ea65dc351e9bb852ca03c082e94d70b929ab9
y-temp4 <y.temp4@gmail.com>
2017-05-19 09:14:59 +0900

    Fix column order in User model

--- db/migrate/20170518071732_sorcery_core.rb
+++ db/migrate/20170518071732_sorcery_core.rb
@@ -2,6 +2,8 @@ class SorceryCore < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :email,            :null => false
+      t.string :username,         :null => false
+
       t.string :crypted_password
       t.string :salt
 
@@ -9,5 +11,6 @@ class SorceryCore < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
   end
 end
--- db/migrate/20170518233648_add_username_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :username, :string, null: false
-
-    add_index :users, :username, unique: true
-  end
-end
