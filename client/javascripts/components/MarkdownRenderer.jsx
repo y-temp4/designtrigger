f79d78b282723ff6f4aa527833f9cfb38a1d95ce
y-temp4 <y.temp4@gmail.com>
2017-05-23 10:25:26 +0900

    Change React.Component extended component to SFC

--- client/javascripts/components/MarkdownRenderer.jsx
+++ client/javascripts/components/MarkdownRenderer.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import remark from 'remark'
 import emoji from 'remark-emoji'
 import reactRenderer from 'remark-react'
 
 const processor = remark().use(reactRenderer).use(emoji)
 
-export default class MarkdownRenderer extends React.Component {
+const MarkdownRenderer = ({ body }) => <div>{processor.processSync(body).contents}</div>
 
-  render() {
-    return (
-      <div>
-        {processor.processSync(this.props.body).contents}
-      </div>
-    )
-  }
+MarkdownRenderer.propTypes = {
+  body: PropTypes.string.isRequired,
 }
+
+export default MarkdownRenderer
