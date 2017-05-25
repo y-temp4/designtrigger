92b6bbcbe201332394641316389901738e1160cd
y-temp4 <y.temp4@gmail.com>
2017-05-25 11:36:01 +0900

    Fix not to show duplicate posts

--- client/javascripts/components/Posts.jsx
+++ client/javascripts/components/Posts.jsx
@@ -8,20 +8,6 @@ export default class Posts extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="container">
-          <ul>
-            {this.props.posts.map((post) => {
-              return (
-                <li key={post.id}>
-                  <Link href={`/@${post.username}/${post.id}`}>
-                    {post.title}
-                  </Link>
-                  <span> by <Link href={`/@${post.username}`}>{post.username}</Link></span>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
         <div className="container-small">
           <div className="row">
             <div className="column-small-10 offset-small-1">
