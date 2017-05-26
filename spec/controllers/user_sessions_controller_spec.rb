f8aa4830e55e0df0e3efc387506406cefd9d200d
y-temp4 <y.temp4@gmail.com>
2017-05-26 13:10:17 +0900

    Add test of user session

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe UserSessionsController, type: :controller do
+  describe 'POST #create' do
+    it "login as new session" do
+      user = create(:user)
+      post :create, params: { email: user.email, password: 'password' }
+      expect(response).to be_success
+      expect(response.body).to eq user.id.to_s
+    end
+
+    it "cannot login" do
+      user = create(:user)
+      post :create, params: { email: user.email, password: 'wrong_password' }
+      expect(response.status).to eq 422
+      expect(response.body).to eq 'ログインに失敗しました'
+    end
+  end
+end
