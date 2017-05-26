5584c5a71420d45e66f362e00784c2359edbbdab
y-temp4 <y.temp4@gmail.com>
2017-05-26 10:01:39 +0900

    Remove unnecessary package

--- client/javascripts/components/Posts.jsx
+++ client/javascripts/components/Posts.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import gravatar from 'gravatar'
 import Layout from './Layout.jsx'
-import Link from './Link.jsx'
 import PostList from './PostList.jsx'
 
 export default class Posts extends React.Component {
