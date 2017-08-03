c00e5f97d80c2b2ffd2ce97f16bfb1e53cf9329e
y-temp4 <y.temp4@gmail.com>
2017-08-03 15:29:37 +0900

    Refactor duplicate code and put it in UserInfo component

--- client/javascripts/components/UserFollowers.jsx
+++ client/javascripts/components/UserFollowers.jsx
@@ -2,70 +2,18 @@ import React from 'react'
 import gravatar from 'gravatar'
 import Layout from './Layout.jsx'
 import Link from './Link.jsx'
-import { sendPost, sendDelete } from '../libs/client-methods.js'
+import UserInfo from './UserInfo.jsx'
 
 export default class UserFollowers extends React.Component {
 
-  handleFollow() {
-    const user_id = this.props.user.id
-
-    sendPost('/follow', { follow: { user_id } })
-  }
-
-  handleUnfollow() {
-    const user_id = this.props.user.id
-
-    sendDelete('/unfollow', { params: { userId: user_id } })
-  }
-
   render() {
-    const { fullPath, currentUser, user, posts, is_following, following_count, follower_count, followers } = this.props
+    const { user, followers } = this.props
 
     return (
       <Layout title={`${user.username}'s followers`}>
         <div className="user">
           <div className="container-small">
-            <div className="row user-profile-box">
-              <div className="column-extra-small-8">
-                <h1 className="user-username">{user.username}</h1>
-                <p className="user-description">{user.description}</p>
-                <a
-                  className="user-website"
-                  href={user.website_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {user.website_url}
-                </a>
-                <p>
-                  <Link href={`/@${user.username}/followees`}>{following_count} Following </Link>
-                  |
-                  <Link href={`/@${user.username}/followers`}> {follower_count} Followers</Link>
-                </p>
-                { currentUser && currentUser.id !== user.id ?
-                  is_following ?
-                    <Link
-                      onClick={this.handleUnfollow.bind(this)}
-                      className="button active"
-                      href={fullPath}
-                    >Following</Link> :
-                    <Link
-                      onClick={this.handleFollow.bind(this)}
-                      className="button"
-                      href={fullPath}
-                    >Follow</Link>
-                  :
-                  null
-                }
-              </div>
-              <div className="column-extra-small-4 user-profile-image-box">
-                <img
-                  className="user-avatar"
-                  src={gravatar.url(user.email, { s: '100' })}
-                  alt={user.username}
-                />
-              </div>
-            </div>
+            <UserInfo {...this.props} />
             <div className="row user-posts-box">
               <div className="column-small-12">
                 <h2>{user.username}{"'"}s followers</h2>
---
+++ client/javascripts/components/UserInfo.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import gravatar from 'gravatar'
+import Link from './Link.jsx'
+import { sendPost, sendDelete } from '../libs/client-methods.js'
+
+export default class UserInfo extends React.Component {
+
+  handleFollow() {
+    const user_id = this.props.user.id
+
+    sendPost('/follow', { follow: { user_id } })
+  }
+
+  handleUnfollow() {
+    const user_id = this.props.user.id
+
+    sendDelete('/unfollow', { params: { userId: user_id } })
+  }
+
+  render() {
+    const { fullPath, currentUser, user, is_following, following_count, follower_count } = this.props
+
+    return (
+      <div className="row user-profile-box">
+        <div className="column-extra-small-8">
+          <h1 className="user-username">{user.username}</h1>
+          <p className="user-description">{user.description}</p>
+          <a
+            className="user-website"
+            href={user.website_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {user.website_url}
+          </a>
+          <p>
+            <Link href={`/@${user.username}/followees`}>{following_count} Following </Link>
+            |
+            <Link href={`/@${user.username}/followers`}> {follower_count} Followers</Link>
+          </p>
+          { currentUser && currentUser.id !== user.id ?
+            is_following ?
+              <Link
+                onClick={this.handleUnfollow.bind(this)}
+                className="button active"
+                href={fullPath}
+              >Following</Link> :
+              <Link
+                onClick={this.handleFollow.bind(this)}
+                className="button"
+                href={fullPath}
+              >Follow</Link>
+            :
+            null
+          }
+        </div>
+        <div className="column-extra-small-4 user-profile-image-box">
+          <img
+            className="user-avatar"
+            src={gravatar.url(user.email, { s: '100' })}
+            alt={user.username}
+          />
+        </div>
+      </div>
+    )
+  }
+}
