3da9749cbc64d65934bfbc37d5aff213c64e374d
y-temp4 <y.temp4@gmail.com>
2017-08-07 19:45:11 +0900

    Refactor to change stateless React Components to pure function

--- client/javascripts/components/UserFollowees.jsx
+++ client/javascripts/components/UserFollowees.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Layout from './Layout.jsx'
 import UserInfo from './UserInfo.jsx'
 import UserList from './UserList.jsx'
 
-export default class UserFollowees extends React.Component {
-  render() {
-    const { user, following_users } = this.props
-    const title = `${user.username}'s following users`
+const UserFollowees = (props) => {
+  const { user, following_users } = props
+  const title = `${user.username}'s following users`
 
-    return (
-      <Layout title={title}>
-        <div className="user">
-          <div className="container-small">
-            <UserInfo {...this.props} />
-            <UserList title={title} user={user} userList={following_users} />
-          </div>
+  return (
+    <Layout title={title}>
+      <div className="user">
+        <div className="container-small">
+          <UserInfo {...props} />
+          <UserList title={title} user={user} userList={following_users} />
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
+
+UserFollowees.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  following_users: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default UserFollowees
--- client/javascripts/components/UserFollowers.jsx
+++ client/javascripts/components/UserFollowers.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Layout from './Layout.jsx'
 import UserInfo from './UserInfo.jsx'
 import UserList from './UserList.jsx'
 
-export default class UserFollowers extends React.Component {
-  render() {
-    const { user, followers } = this.props
-    const title = `${user.username}'s followers`
+const UserFollowers = (props) => {
+  const { user, followers } = props
+  const title = `${user.username}'s followers`
 
-    return (
-      <Layout title={title}>
-        <div className="user">
-          <div className="container-small">
-            <UserInfo {...this.props} />
-            <UserList title={title} user={user} userList={followers} />
-          </div>
+  return (
+    <Layout title={title}>
+      <div className="user">
+        <div className="container-small">
+          <UserInfo {...props} />
+          <UserList title={title} user={user} userList={followers} />
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
+
+UserFollowers.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  followers: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default UserFollowers
