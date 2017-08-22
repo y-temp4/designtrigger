d227405cd1a5fc625b078f2064e9e14f145a6fb2
y-temp4 <y.temp4@gmail.com>
2017-08-22 14:21:25 +0900

    Use stateless component and Add type at User component

--- client/javascripts/components/User.jsx
+++ client/javascripts/components/User.jsx
@@ -1,40 +1,52 @@
 import React from 'react'
-import gravatar from 'gravatar'
+import PropTypes from 'prop-types'
 import Layout from './Layout.jsx'
 import Link from './Link.jsx'
 import UserInfo from './UserInfo.jsx'
 
-export default class User extends React.Component {
+const User = (props) => {
+  const { user, posts } = props
 
-  render() {
-    const { user, posts } = this.props
-
-    return (
-      <Layout title={user.username}>
-        <div className="user">
-          <div className="container-small">
-            <UserInfo {...this.props} />
-            <div className="row user-posts-box">
-              <div className="column-small-12">
-                <h2>{user.username}{"'"}s posts</h2>
-                { posts.length !== 0 ?
-                  posts.map(post => (
-                    <Link key={post.id} href={`/@${user.username}/posts/${post.uuid}`}>
-                      <div className="user-post-box">
-                        <h2 className="post-title">
-                          {post.title}
-                        </h2>
-                        <span className="post-body">
-                          {new Date(post.created_at).toDateString()}
-                        </span>
-                      </div>
-                    </Link>),
-                  ) : <p>No posts yet.</p>}
-              </div>
+  return (
+    <Layout title={user.username}>
+      <div className="user">
+        <div className="container-small">
+          <UserInfo {...props} />
+          <div className="row user-posts-box">
+            <div className="column-small-12">
+              <h2>{user.username}{"'"}s posts</h2>
+              { posts.length !== 0 ?
+                posts.map(post => (
+                  <Link key={post.id} href={`/@${user.username}/posts/${post.uuid}`}>
+                    <div className="user-post-box">
+                      <h2 className="post-title">
+                        {post.title}
+                      </h2>
+                      <span className="post-body">
+                        {new Date(post.created_at).toDateString()}
+                      </span>
+                    </div>
+                  </Link>),
+                ) : <p>No posts yet.</p>}
             </div>
           </div>
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
+
+User.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  posts: PropTypes.arrayOf(
+    PropTypes.object,
+  ),
+}
+
+User.defaultProps = {
+  posts: [],
+}
+
+export default User
