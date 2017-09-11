fd8e6dd161a1eea1fd6570ed11cfc4fd490ea224
y-temp4 <y.temp4@gmail.com>
2017-09-11 13:02:31 +0900

    Show user icon at follower/followee page

--- client/javascripts/components/UserList.jsx
+++ client/javascripts/components/UserList.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import gravatar from 'gravatar'
 import Link from './Link.jsx'
 
 const UserList = ({ title, userList }) => (
-  <div className="row user-posts-box">
+  <div className="row user-list-box">
     <div className="column-small-12">
       <h2>{title}</h2>
       {userList.map(user => (
         <Link key={user.id} href={`/@${user.username}`}>
-          <div className="user-post-box">
-            {user.username}
+          <div className="user-box">
+            <div className="container-max">
+              <div className="column-extra-small-1" style={{ height: '30px', paddingRight: '1.5rem' }}>
+                <img
+                  className="header-avatar"
+                  src={gravatar.url(user.email, { s: '30' })}
+                  alt={user.username}
+                />
+              </div>
+              <div className="column-extra-small-10" style={{ marginTop: '2px' }}>
+                {user.username}
+              </div>
+            </div>
           </div>
         </Link>),
       )}
