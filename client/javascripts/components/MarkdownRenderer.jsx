21011a6953af77d92210c93692a6e14efd46cf97
y-temp4 <y.temp4@gmail.com>
2017-09-12 12:51:32 +0900

    Fix bug that can't be sent post when language of code block is set to nonexistent language

--- client/javascripts/components/MarkdownRenderer.jsx
+++ client/javascripts/components/MarkdownRenderer.jsx
@@ -4,19 +4,20 @@ import emoji from 'remark-emoji'
 import unified from 'unified'
 import markdown from 'remark-parse'
 import remark2rehype from 'remark-rehype'
-import highlight from 'rehype-highlight'
+import highlight from 'remark-highlight.js'
 import rehype2react from 'rehype-react'
 import row from 'rehype-raw'
 
 const processor = unified()
   .use(markdown)
+  .use(highlight)
   .use(emoji)
   .use(remark2rehype, { allowDangerousHTML: true })
   .use(row)
-  .use(highlight)
   .use(rehype2react, {
     createElement: React.createElement,
   })
+
 
 const MarkdownRenderer = ({ body }) => <div>{processor.processSync(body).contents}</div>
 
