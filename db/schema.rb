4e8db11196f9e28e3ae50ad3fca54365a3d75fb5
y-temp4 <y.temp4@gmail.com>
2017-10-03 13:33:22 +0900

    Add profile_image_url column to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927033713) do
+ActiveRecord::Schema.define(version: 20171003042409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170927033713) do
     t.string "description"
     t.string "website_url"
     t.integer "uploaded_image_size", default: 0
+    t.string "profile_image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
