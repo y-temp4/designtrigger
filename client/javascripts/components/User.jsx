47f4c379bc33b4ef142987d25566a0a32ef633db
y-temp4 <y.temp4@gmail.com>
2017-11-15 15:10:18 +0900

    Add feature to edit and delete posts on user page

--- client/javascripts/components/User.jsx
+++ client/javascripts/components/User.jsx
@@ -3,50 +3,80 @@ import PropTypes from 'prop-types'
 import Layout from './Layout.jsx'
 import Link from './Link.jsx'
 import UserInfo from './UserInfo.jsx'
+import { sendDelete } from '../libs/client-methods.js'
 
-const User = (props) => {
-  const { user, posts } = props
+export default class User extends React.Component {
+  static defaultProps = {
+    posts: [],
+    currentUser: null,
+  }
 
-  return (
-    <Layout title={user.username}>
-      <div className="user">
-        <div className="container-small">
-          <UserInfo {...props} />
-          <div className="row user-posts-box">
-            <div className="column-small-12">
-              <h2>{user.username}{"'"}s posts</h2>
-              { posts.length !== 0 ?
-                posts.map(post => (
-                  <Link key={post.id} href={`/@${user.username}/posts/${post.uuid}`}>
-                    <div className="user-post-box">
-                      <h2 className="post-title">
-                        {post.title}
-                      </h2>
+  static propTypes = {
+    user: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+    }).isRequired,
+    posts: PropTypes.arrayOf(
+      PropTypes.object,
+    ),
+    currentUser: PropTypes.shape({
+      username: PropTypes.string,
+    }),
+  }
+
+  handleDeletePost(event, post) {
+    const { currentUser } = this.props
+
+    event.stopPropagation()
+    event.nativeEvent.stopImmediatePropagation()
+
+    if (window.confirm('削除してもよろしいですか？')) {
+      sendDelete(`/@${currentUser.username}/posts/${post.uuid}`).then(() => {
+        location.href = `/@${currentUser.username}`
+      })
+    }
+  }
+
+  render() {
+    const { user, posts, currentUser } = this.props
+
+    return (
+      <Layout title={user.username}>
+        <div className="user">
+          <div className="container-small">
+            <UserInfo {...this.props} />
+            <div className="row user-posts-box">
+              <div className="column-small-12">
+                <h2>{user.username}{"'"}s posts</h2>
+                { posts.length !== 0 ?
+                  posts.map(post => (
+                    <div key={post.id} className="user-post-box">
+                      <Link href={`/@${user.username}/posts/${post.uuid}`}>
+                        <h2 className="post-title">
+                          {post.title}
+                        </h2>
+                      </Link>
                       <span className="post-body">
                         {new Date(post.created_at).toDateString()}
+                        {
+                          currentUser !== null && post.user_id === currentUser.id &&
+                          <span>
+                            <Link
+                              href={`/@${currentUser.username}/posts/${post.uuid}/edit`}
+                            > 編集する</Link>
+                            <Link
+                              href={`/@${currentUser.username}/posts/${post.uuid}`}
+                              onClick={e => this.handleDeletePost(e, post)}
+                            > 記事を削除する</Link>
+                          </span>
+                        }
                       </span>
-                    </div>
-                  </Link>),
-                ) : <p>No posts yet.</p>}
+                    </div>),
+                  ) : <p>No posts yet.</p>}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
-
-User.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-  }).isRequired,
-  posts: PropTypes.arrayOf(
-    PropTypes.object,
-  ),
-}
-
-User.defaultProps = {
-  posts: [],
-}
-
-export default User
