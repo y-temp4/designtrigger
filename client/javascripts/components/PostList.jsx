3b428ca8652263adbdd6226ad6e5dd1c1b502210
y-temp4 <y.temp4@gmail.com>
2017-11-17 09:17:17 +0900

    Show message if there are no posts

--- client/javascripts/components/PostList.jsx
+++ client/javascripts/components/PostList.jsx
@@ -7,37 +7,38 @@ const PostList = ({ posts, title }) => (
     <div className="row">
       <div className="column-small-10 offset-small-1">
         <h2>{title}</h2>
-        {posts.map(post => (
-          <div key={post.id} className="user-post-box">
-            <div className="container-max">
-              <div className="row">
-                <div className="column-extra-small-1" style={{ padding: 0, width: '15px' }}>
-                  <Link href={`/@${post.user.username}`}>
-                    <img
-                      className="header-avatar"
-                      src={post.user.profile_image_url}
-                      alt={post.user.username}
-                      style={{ width: '25px' }}
-                    />
-                  </Link>
-                </div>
-                <div className="column-extra-small-1">
-                  <Link key={post.id} href={`/@${post.user.username}`}>
-                    {post.user.username}
-                  </Link>
+        { posts.length !== 0 ?
+          posts.map(post => (
+            <div key={post.id} className="user-post-box">
+              <div className="container-max">
+                <div className="row">
+                  <div className="column-extra-small-1" style={{ padding: 0, width: '15px' }}>
+                    <Link href={`/@${post.user.username}`}>
+                      <img
+                        className="header-avatar"
+                        src={post.user.profile_image_url}
+                        alt={post.user.username}
+                        style={{ width: '25px' }}
+                      />
+                    </Link>
+                  </div>
+                  <div className="column-extra-small-1">
+                    <Link key={post.id} href={`/@${post.user.username}`}>
+                      {post.user.username}
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-            <Link key={post.id} href={`/@${post.user.username}/posts/${post.uuid}`}>
-              <h2 className="post-title">
-                {post.title}
-              </h2>
-            </Link>
-            <span className="post-body">
-              {new Date(post.created_at).toDateString()}
-            </span>
-          </div>),
-        )}
+              <Link key={post.id} href={`/@${post.user.username}/posts/${post.uuid}`}>
+                <h2 className="post-title">
+                  {post.title}
+                </h2>
+              </Link>
+              <span className="post-body">
+                {new Date(post.created_at).toDateString()}
+              </span>
+            </div>),
+          ) : <p>記事がありません。</p>}
       </div>
     </div>
   </div>
