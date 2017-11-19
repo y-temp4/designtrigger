1977ce393d3bb9fbd81c6423bae04398a1368c0c
y-temp4 <y.temp4@gmail.com>
2017-11-19 12:59:40 +0900

    Fix for heroku

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -22,7 +22,7 @@ ReactOnRails.configure do |config|
 
   # This configures the script to run to build the production assets by webpack. Set this to nil
   # if you don't want react_on_rails building this file for you.
-  config.build_production_command = "yarn run build:production"
+  config.build_production_command = "cd client && yarn run build:production"
 
   ################################################################################
   # CLIENT RENDERING OPTIONS
