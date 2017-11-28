94fa2421cf1b42c0d40005f4e415eb0a80d1b357
y-temp4 <y.temp4@gmail.com>
2017-11-28 23:05:31 +0900

    Update to login automatically after user activation

--- app/controllers/user_activations_controller.rb
+++ app/controllers/user_activations_controller.rb
@@ -2,7 +2,8 @@ class UserActivationsController < ApplicationController
   def create
     if (@user = User.load_from_activation_token(params[:id]))
       @user.activate!
-      redirect_to(login_path, notice: 'User was successfully activated.')
+      auto_login @user
+      redirect_to root_path
     else
       not_authenticated
     end
