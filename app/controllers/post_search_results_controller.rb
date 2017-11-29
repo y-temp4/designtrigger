2ad5d5d5a15c064ee53e7785201ef90a5debf36d
y-temp4 <y.temp4@gmail.com>
2017-11-29 13:51:40 +0900

    Include body and tag in search target

--- app/controllers/post_search_results_controller.rb
+++ app/controllers/post_search_results_controller.rb
@@ -1,8 +1,10 @@
 class PostSearchResultsController < ApplicationController
   def show
     keywords = params[:q].split(/[\p{blank}\s]+/)
-    groupings = keywords.reduce({}){ |acc, elem| acc.merge(elem => { title_cont: elem })}
-    posts = Post.ransack(combinator: 'and', groupings: groupings).result
+    groupings = keywords.reduce({}) do |acc, elem|
+      acc.merge(elem => { title_or_body_or_tags_name_cont: elem })
+    end
+    posts = Post.ransack(combinator: 'and', groupings: groupings).result(distinct: true)
 
     posts_with_username = posts.includes(:user).order(created_at: :desc).map do |post|
       post.as_json.merge(user: post.user.as_json)
